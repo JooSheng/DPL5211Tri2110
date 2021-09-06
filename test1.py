27940b92deb637a70aa0c341be5cee864c882b7a
JooSheng <90165778+JooSheng@users.noreply.github.com>
2021-09-06 10:40:02 +0800

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,7 @@
+#student ID:1201200987#
+
+print("Hi python")
+person = input("Please enter youe name :")
+print("Hello",person)
+
+#Lab 1.2 get input two numbers and display the sum of the two numbers.
