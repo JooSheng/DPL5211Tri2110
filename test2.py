8630d63780a51cbd9c210681d34f7bacef9375e3
JooSheng <90165778+JooSheng@users.noreply.github.com>
2021-09-06 10:37:27 +0800

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,13 @@
+#student ID:1201200987#
+
+
+number1 = int(input("Please enter number 1 :"))
+number2 = int(input("Please enter number 2 :"))
+
+Tnumber = number1 + number2
+
+print("Total of ",number1," and ",number2," is ",Tnumber)
+print("The sum of {} and {} is {}".format(number1,number2,Tnumber))
+
+#print("The total number is ",total)
+#Display --> The sum of 5 and 6 is 11
